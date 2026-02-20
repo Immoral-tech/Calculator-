94fee9e53ddd67b29c8fb4b29940f7846c1c61ff
Артем Янковский <a.p.yankovskiy@mail.ru>
2026-02-20 21:10:40 +0300

    Добавил консольный калькулятор

---
+++ Calculator/Program.cs
@@ -0,0 +1,90 @@
+﻿Console.WriteLine("Первая пограмма на C#");
+Console.WriteLine("Консольный калькулятор");
+Console.WriteLine();
+
+//Ввод первого числа
+Console.WriteLine("Введите первое число: ");
+string? input1 = Console.ReadLine();
+double number1 = 0;
+
+if (!double.TryParse(input1, out number1))
+{
+    Console.WriteLine("Ошибка! Это не число. Программа завершена.");
+    return;
+}
+
+//Ввод второго числа
+Console.WriteLine("Введите второе число");
+string? input2 = Console.ReadLine();
+
+if (!double.TryParse(input2, out double number2))
+{
+    Console.WriteLine("Ошибка! Это не число. Программа завершена.");
+    return;
+}
+
+//Ввод операций
+Console.WriteLine("Выберите операцию: ");
+Console.WriteLine("1 - Сложение (+)");
+Console.WriteLine("2 - Вычитание (-)");
+Console.WriteLine("3 - Умножение (*)");
+Console.WriteLine("4 - Деление (/)");
+Console.WriteLine("Ваш выбор (1-4): ");
+
+string? operationChoice = Console.ReadLine();
+
+//Выполнение операции
+double result = 0;
+bool validOperation = true;
+
+if (operationChoice == "1")
+{
+    result = number1 + number2;
+    Console.WriteLine($"{number1} + {number2} = {result}");
+}
+
+else if (operationChoice == "2")
+{
+    result = number1 - number2;
+    Console.WriteLine($"{number1} - {number2} = {result}");
+}
+
+else if (operationChoice == "3")
+{
+    result = number1 * number2;
+    Console.WriteLine($"{number1} * {number2} = {result}");
+}
+
+else if (operationChoice == "4")
+{
+    if (number2 != 0)
+    {
+        result = number1 / number2;
+        Console.WriteLine($"{number1} / {number2} = {result}");
+    }
+    else
+    {
+        Console.WriteLine("Ошибка! Деление на ноль невозможно.");
+        validOperation = false;
+    }
+}
+
+else
+{
+    Console.WriteLine("Ошибка! Неверный выбор операции.");
+    validOperation = false;
+
+}
+
+//Итоговое сообщение
+if (validOperation)
+{
+    Console.WriteLine("Операция выполнена успешно!");
+}
+else
+{
+    Console.WriteLine("Не удалось выполнить операцию.");
+}
+
+Console.WriteLine("Нажмите любую клавишу для выхода...");
+Console.ReadKey();
