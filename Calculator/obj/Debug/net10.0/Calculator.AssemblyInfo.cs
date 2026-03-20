4c4096c3e419473f576eac03990d0e46dfb4fbe8
Артем Янковский <a.p.yankovskiy@mail.ru>
2026-03-20 17:32:17 +0300

    Переделал структуру сложения, вычитания, умножения, деления

--- Calculator/obj/Debug/net10.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net10.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5fc12af3672bb4517b6a9a5afe3b0a36cc39b6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ed4ced45bd7e423beacf3c23631acbde7cce39c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
